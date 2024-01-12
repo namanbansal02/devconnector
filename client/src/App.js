d552d241f672f7e0fb9112a67eb6d890b268c144
Shwetakshi Priya <shwetakshi1175.be21@chitkarauniversity.edu.in>
2024-01-12 11:02:42 +0530

    shwetakshi's try

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,46 @@
-import {useEffect} from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
-import {loadUser} from './action/auth';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import { loadUser } from "./action/auth";
+import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 
-import { Provider } from 'react-redux';
-import store from './store';
-import './App.css';
-import setAuthToken from './utils/setAuthToken';
+import { Provider } from "react-redux";
+import store from "./store";
+import "./App.css";
+import setAuthToken from "./utils/setAuthToken";
 
-
-
-if(localStorage.token){
+if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
 
 const App = () => (
-
   useEffect(() => {
     store.dispatch(loadUser());
-  }
-  , []),
-
-  <Provider store={store}>
-    <Router>
-   
+  }, []),
+  (
+    <Provider store={store}>
+      <Router>
         <Navbar />
-     
+        <Alert />
 
-        <Alert/>
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route
+            path="dashboard"
+            element={<PrivateRoute component={Dashboard} />}
+          />
         </Routes>
-
-    
-   
-    </Router>
-  </Provider>
+      </Router>
+    </Provider>
+  )
 );
+
 export default App;
